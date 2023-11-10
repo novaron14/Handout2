3222d8a57cec27b63a60c7782f2051593b0e3c2a
novaron14 <novaron14@gmail.com>
2023-11-10 22:14:05 +0100

    first iteration

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 export function greet(names: string[]): string {
-    if (names.length === 0) {
-      return 'Hello, my friend';
-    }
-  
-    return '';
+  if (names.length === 0) {
+    return 'Hello, my friend';
   }
+
+  if (names.length === 1) {
+    return `Hello ${names[0]}.`;
+  }
+
+  const lastPerson = names.pop();
+  const allNames = names.join(', ');
+
+  return `Hello ${allNames} and ${lastPerson}.`;
+}
--- tests/app.test.ts
+++ tests/app.test.ts
@@ -3,3 +3,12 @@ import { greet } from "../src/app";
 test('handles no input', () => {
   expect(greet([])).toBe('Hello, my friend');
 });
+
+test('handles single input', () => {
+  expect(greet(['Bob'])).toBe('Hello Bob.');
+});
+
+test('handles multiple inputs', () => {
+  expect(greet(['Alice', 'Bob', 'Jerry'])).toBe('Hello Alice, Bob and Jerry.');
+});
+
